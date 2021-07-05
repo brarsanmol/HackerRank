16475a562f51b8f2e5060a8393b4663a896817f5
Anmol Brar <brarsanmol@protonmail.com>
2021-07-05 13:07:57 -0400

    Add Fifth C++ Exercise.

---
+++ C++/5.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int min, max {0};
+    cin >> min >> max;
+
+    for (int number {min}; number <= max; number++) {      
+        if (number == 0) {
+            cout << "zero" << endl;
+        } else if (number == 1) {
+            cout << "one" << endl;
+        } else if (number == 2) {
+            cout << "two" << endl;
+        } else if (number == 3) {
+            cout << "three" << endl;
+        } else if (number == 4) {
+            cout << "four" << endl;
+        } else if (number == 5) {
+            cout << "five" << endl;
+        } else if (number == 6) {
+            cout << "six" << endl;
+        } else if (number == 7) {
+            cout << "seven" << endl;
+        } else if (number == 8) {
+            cout << "eight" << endl;
+        } else if (number == 9) {
+            cout << "nine" << endl;
+        } else {
+            if (number % 2 == 0) {
+                cout << "even" << endl;
+            } else {
+                cout << "odd" << endl;
+            }
+        }
+    }
+}
